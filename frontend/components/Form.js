3494aefd00679fcc092edd69252ed5252929b89d
Steven Fehr <shfehr@gmail.com>
2024-01-16 11:35:01 -0600

    Done - phew

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -39,9 +39,9 @@ export function Form(props) {
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
+      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" value = {form.newQuestion}/>
+      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" value = {form.newTrueAnswer}/>
+      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" value = {form.newFalseAnswer}/>
       <button id="submitNewQuizBtn"
               disabled = {isDisabled()}
               >Submit new quiz</button>
